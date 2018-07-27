5f4094517d03ceb963b7afcefd988b5b15e3fcb3
WillNess210 <WillNess210@gmail.com>
2018-07-26 23:42:09 -0600

    Added text to speech
    - still needs improvement on the speech
    - button needs to be themed and properly placed

--- Oodler/app/src/main/java/com/example/studyapp/oodler/DisplayMessageActivity.java
+++ Oodler/app/src/main/java/com/example/studyapp/oodler/DisplayMessageActivity.java
@@ -2,16 +2,24 @@ package com.example.studyapp.oodler;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 
-public class DisplayMessageActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class DisplayMessageActivity extends AppCompatActivity implements TextToSpeech.OnInitListener{
     private AdView mAdView;
+    private TextToSpeech tts;
+    private Button btnSpeak;
+    private String toSpeak;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,10 +43,58 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView);
+
+        // TEXT TO SPEECH
+        tts = new TextToSpeech(this, this);
+        btnSpeak = (Button) findViewById(R.id.btnSpeak);
+        toSpeak = toDisplay;
+        // button on click event
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                speakOut();
+            }
+        });
+
+        // CHANGING TEXT
         textView.setText(toDisplay);
+
+        // AD
         MobileAds.initialize(this, "@string/adid");
         mAdView = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
+    }
+    @Override
+    public void onDestroy() {
+        // Don't forget to shutdown tts!
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public void onInit(int status) {
+
+        if (status == TextToSpeech.SUCCESS) {
+
+            int result = tts.setLanguage(Locale.US);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+            } else {
+                btnSpeak.setEnabled(true);
+                speakOut();
+            }
+
+        } else {
+        }
+
+    }
+
+    private void speakOut() {
+        tts.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
     }
 }
